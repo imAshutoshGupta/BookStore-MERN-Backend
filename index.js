60a9746339e67f03c3a69c57252be73718b78a46
imAshutoshGupta <aguptaworkspace@gmail.com>
2024-10-22 02:34:29 +0530

    url update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://book-store-mern-frontend-orcin.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://book-store-mern-frontend-orcin.vercel.app'],
     credentials: true
 }))
 
